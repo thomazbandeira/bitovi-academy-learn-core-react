50e3e1e87fef3a93410be1a639aae55046279d91
Thomaz Bandeira <thomaz.bandeira@avanade.com>
2024-03-26 01:45:02 -0300

    fix: exercise is done. Test is passed.

--- place-my-order/src/pages/Home/Home.tsx
+++ place-my-order/src/pages/Home/Home.tsx
@@ -1,4 +1,6 @@
 import HeroImage from "place-my-order-assets/images/homepage-hero.jpg"
+import { Link } from "react-router-dom"
+import styles from "./Home.module.css"
 
 const Home: React.FC = () => {
     return (
@@ -15,6 +17,12 @@ const Home: React.FC = () => {
             <p>
                 We make it easier than ever to order gourmet food from your favorite
                 local restaurants.
+            </p>
+
+            <p>
+                <Link className={styles.chooseButton} to="/restaurants">
+                    Choose a Restaurant
+                </Link>
             </p>
         </div>
     )
